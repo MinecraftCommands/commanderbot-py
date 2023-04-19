a02f3b92298fa3b7b0ae0b774fd8db3175444e5b
Ersatz <cm12mc@gmail.com>
2023-04-19 13:23:19 -0700

    Removed special case exception handling for defers

--- commanderbot/lib/responsive_exception.py
+++ commanderbot/lib/responsive_exception.py
@@ -1,17 +1,7 @@
-import datetime
 from typing import Optional, Union
 
-from discord import (
-    AllowedMentions,
-    Interaction,
-    InteractionMessage,
-    InteractionResponseType,
-    WebhookMessage,
-)
+from discord import AllowedMentions, Interaction
 from discord.ext.commands import Context
-
-from commanderbot.lib.utils.datetimes import datetime_to_int
-from commanderbot.lib.utils.utils import utcnow_aware
 
 __all__ = ("ResponsiveException",)
 
@@ -52,39 +42,7 @@ class ResponsiveException(Exception):
             )
             return
 
-        # Handle app command errors that had their interaction responded to without a defer
-        if context.response.type not in {
-            InteractionResponseType.deferred_channel_message,
-            InteractionResponseType.deferred_message_update,
-        }:
-            await context.followup.send(
-                str(self), allowed_mentions=allowed_mentions, ephemeral=True
-            )
-            return
-
-        # Handle app command errors that had their interaction deferred
-        original_response: InteractionMessage = await context.original_response()
-        if original_response.flags.ephemeral:
-            # Handle ephemeral defers by just sending an ephemeral followup
-            await context.followup.send(
-                str(self), allowed_mentions=allowed_mentions, ephemeral=True
-            )
-        else:
-            # Handle regular defers by sending a followup that everyone can see
-            # The followup will be deleted after a small delay
-            deleting_in_ts: int = datetime_to_int(
-                utcnow_aware() + datetime.timedelta(seconds=11)
-            )
-            deleting_in_msg: str = (
-                f"(*This message will self destruct in <t:{deleting_in_ts}:R>*)"
-            )
-            message: WebhookMessage = await context.followup.send(
-                f"{self}\n{deleting_in_msg}",
-                allowed_mentions=allowed_mentions,
-                wait=True,
-            )
-            
-            try:
-                await message.delete(delay=10)
-            except:
-                pass
+        # Handle app command errors that had their interaction responded to
+        await context.followup.send(
+            str(self), allowed_mentions=allowed_mentions, ephemeral=True
+        )
