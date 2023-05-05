1318b51fa26d107858c4958bc940f824126c5c17
Ersatz <cm12mc@gmail.com>
2023-05-04 23:28:16 -0700

    Improved button based confirmation dialog

--- commanderbot/lib/dialogs.py
+++ commanderbot/lib/dialogs.py
@@ -3,8 +3,9 @@ from enum import Enum
 from typing import Optional
 
 import discord
-from discord import ButtonStyle, Interaction, Member, Message, Reaction, WebhookMessage
+from discord import ButtonStyle, Interaction, Member, Message, Reaction
 from discord.ext.commands import Bot, Context
+from discord.interactions import Interaction
 from discord.ui import Button, View, button
 
 from commanderbot.lib import AllowedMentions
@@ -12,7 +13,8 @@ from commanderbot.lib import AllowedMentions
 __all__ = (
     "ConfirmationResult",
     "confirm_with_reaction",
-    "confirm_with_buttons",
+    "ConfirmView",
+    "respond_with_confirmation",
 )
 
 
@@ -65,8 +67,8 @@ async def confirm_with_reaction(
 
     # If an appropriate reaction is not received soon enough, assume "no."
     except asyncio.TimeoutError:
-        await conf_message.remove_reaction(reaction_yes, bot.user)
-        await conf_message.remove_reaction(reaction_no, bot.user)
+        await conf_message.remove_reaction(reaction_yes, bot.user)  # type: ignore
+        await conf_message.remove_reaction(reaction_no, bot.user)  # type: ignore
         return ConfirmationResult.NO_RESPONSE
 
     # Otherwise, check which reaction was applied.
@@ -74,103 +76,103 @@ async def confirm_with_reaction(
         assert isinstance(conf_reaction, Reaction)
         # Check if the response is a "yes."
         if str(conf_reaction.emoji) == reaction_yes:
-            await conf_message.remove_reaction(reaction_no, bot.user)
+            await conf_message.remove_reaction(reaction_no, bot.user)  # type: ignore
             return ConfirmationResult.YES
 
     # If we get this far, the answer is an explicit "no."
-    await conf_message.remove_reaction(reaction_yes, bot.user)
+    await conf_message.remove_reaction(reaction_yes, bot.user)  # type: ignore
     return ConfirmationResult.NO
 
 
 class ConfirmView(View):
-    def __init__(
-        self,
-        original_interaction: Interaction,
-        timeout: int = 180,
-    ):
-        self.original_interaction = original_interaction
+    def __init__(self, interaction: Interaction, timeout: Optional[float] = None):
+        self.original_interaction: Interaction = interaction
         self.result = ConfirmationResult.NO_RESPONSE
-        self.followup_message: Optional[WebhookMessage] = None
         super().__init__(timeout=timeout)
 
-    @button(label="Yes", style=ButtonStyle.green)
-    async def yes_callback(self, interaction: Interaction, button: Button):
-        self.result = ConfirmationResult.YES
-        # Run `_on_confirm()` before deferring so the buttons and view doesn't get re-enabled for a short time
-        await self._on_confirm(button)
-        # Defer the interaction (Mostly to prevent exceptions)
-        await interaction.response.defer()
-
-    @button(label="No", style=ButtonStyle.red)
-    async def no_callback(self, interaction: Interaction, button: Button):
-        self.result = ConfirmationResult.NO
-        # Run `_on_confirm()` before deferring so the buttons and view doesn't get re-enabled for a short time
-        await self._on_confirm(button)
-        # Defer the interaction (Mostly to prevent exceptions)
-        await interaction.response.defer()
-
+    # @overrides View
     async def interaction_check(self, interaction: Interaction) -> bool:
-        # Only allow the user, who this view is for, to interact with it
-        if self.original_interaction.user != interaction.user:
+        if interaction.user != self.original_interaction.user:
             await interaction.response.send_message(
-                "😠 This confirmation dialog isn't for you!", ephemeral=True
+                "😠 This confirmation dialog isn't for you", ephemeral=True
             )
             return False
         return True
 
+    # @overrides View
     async def on_timeout(self):
         # Disable all buttons and set their color to gray
-        for item in self.children:
-            assert isinstance(item, Button)
-            item.style = ButtonStyle.gray
-            item.disabled = True
+        for button in self.children:
+            assert isinstance(button, Button)
+            button.style = ButtonStyle.gray
+            button.disabled = True
 
-        # Edit the view
-        assert self.followup_message
-        await self.followup_message.edit(view=self)
+        # Attempt to edit the view (The original response may have been deleted)
+        try:
+            await self.original_interaction.edit_original_response(view=self)
+        except:
+            pass
 
-    async def _on_confirm(self, button: Button):
+    @button(
+        label="Yes",
+        style=ButtonStyle.green,
+        custom_id="commanderbot_lib:dialogs.confirm_view.yes",
+    )
+    async def yes_callback(self, interaction: Interaction, button: Button):
+        self.result = ConfirmationResult.YES
+        await self._on_confirm(interaction, button)
+
+    @button(
+        label="No",
+        style=ButtonStyle.red,
+        custom_id="commanderbot_lib:dialogs.confirm_view.no",
+    )
+    async def no_callback(self, interaction: Interaction, button: Button):
+        self.result = ConfirmationResult.NO
+        await self._on_confirm(interaction, button)
+
+    async def _on_confirm(self, interaction: Interaction, pressed_button: Button):
+        # Remove all buttons except for the one that was interacted with
+        for button in self.children[:]:
+            assert isinstance(button, Button)
+            if button is not pressed_button:
+                self.remove_item(button)
+
+        # Disable the button that was interacted with
+        pressed_button.disabled = True
+
+        # Attempt to edit the view (The original response may have been deleted)
+        try:
+            await interaction.response.edit_message(view=self)
+        except:
+            pass
+
         # Stop accepting inputs
         self.stop()
 
-        # Remove all buttons except for the one interacted with
-        for item in self.children[:]:
-            assert isinstance(item, Button)
-            if item is not button:
-                self.remove_item(item)
 
-        # Disable the button that was interacted with
-        button.disabled = True
-
-        # Edit the view
-        assert self.followup_message
-        await self.followup_message.edit(view=self)
-
-
-async def confirm_with_buttons(
+async def respond_with_confirmation(
     interaction: Interaction,
     content: str,
     *,
-    timeout: int = 60,
-    ephemeral=True,
+    timeout: float = 60.0,
+    ephemeral=False,
     allowed_mentions: discord.AllowedMentions = discord.AllowedMentions.none()
-):
+) -> ConfirmationResult:
     """
     Ask a user to confirm an action via a `discord.ui.View` with buttons.
 
-    A response will be sent as a followup message to `interaction` composed of the
-    given `content`. The view will wait for up to `timeout` seconds for a "yes" or
-    "no" response. If no response is received, "no response" will be returned.
+    The `content` and confirmation view will be sent as a response to `interaction`.
+    The view will wait for up to `timeout` seconds for a "yes" or "no" response.
+    If no response is received, "no response" will be returned.
     """
 
-    # Create the view and send it as a followup
+    # Create the view and send it as a response
     view = ConfirmView(interaction, timeout)
-    view.followup_message = await interaction.followup.send(
+    await interaction.response.send_message(
         content, view=view, ephemeral=ephemeral, allowed_mentions=allowed_mentions
     )
 
-    # Wait for the view to have an interaction or a timeout
+    # Wait for the view to be interacted with or time out then return the result
     await view.wait()
-
-    # Return the result
     return view.result
