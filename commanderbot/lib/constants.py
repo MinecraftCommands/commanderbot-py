0e7b66ecd37440533b3ea2dc2fd506327e40962b
Ersatz <cm12mc@gmail.com>
2023-11-17 12:39:37 -0800

    Added constants for bot version, discord.py version, python version, and user agent

--- commanderbot/lib/constants.py
+++ commanderbot/lib/constants.py
@@ -1,8 +1,25 @@
+import sys
+from importlib.metadata import version
+
 __all__ = (
+    "COMMANDERBOT_VERSION",
+    "DISCORD_PY_VERSION",
+    "PYTHON_VERSION",
+    "USER_AGENT",
     "MAX_MESSAGE_LENGTH",
     "MAX_EMBED_TITLE_LENGTH",
     "MAX_EMBED_DESCRIPTION_LENGTH",
     "MAX_AUTOCOMPLETE_CHOICES",
+)
+
+COMMANDERBOT_VERSION: str = version("commanderbot")
+DISCORD_PY_VERSION: str = version("discord.py")
+PYTHON_VERSION: str = (
+    f"{sys.version_info[0]}.{sys.version_info[1]}.{sys.version_info[2]}"
+)
+
+USER_AGENT: str = (
+    f"CommanderBuff/{COMMANDERBOT_VERSION} (By the Minecraft Commands community)"
 )
 
 MAX_MESSAGE_LENGTH: int = 2000
