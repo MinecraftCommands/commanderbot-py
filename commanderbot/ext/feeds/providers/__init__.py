69f9c2230d4a1b745e6ef96c2a422300b85886d4
Ersatz <cm12mc@gmail.com>
2024-02-13 01:03:01 -0800

    Changed enum values

--- commanderbot/ext/feeds/providers/__init__.py
+++ commanderbot/ext/feeds/providers/__init__.py
@@ -5,12 +5,12 @@ from commanderbot.ext.feeds.providers.utils import *
 
 
 class FeedProviderType(Enum):
-    minecraft_java_updates = "Minecraft Java Updates"
-    minecraft_bedrock_updates = "Minecraft Bedrock Updates"
+    minecraft_java_updates = "Minecraft: Java Edition Updates"
+    minecraft_bedrock_updates = "Minecraft: Bedrock Edition Updates"
 
 
 class FeedType(Enum):
-    minecraft_java_releases = "Minecraft Java Releases"
-    minecraft_java_snapshots = "Minecraft Java Snapshots"
-    minecraft_bedrock_releases = "Minecraft Bedrock Releases"
-    minecraft_bedrock_previews = "Minecraft Bedrock Previews"
+    minecraft_java_releases = "Minecraft: Java Edition Releases"
+    minecraft_java_snapshots = "Minecraft: Java Edition Snapshots"
+    minecraft_bedrock_releases = "Minecraft: Bedrock Edition Releases"
+    minecraft_bedrock_previews = "Minecraft: Bedrock Edition Previews"
