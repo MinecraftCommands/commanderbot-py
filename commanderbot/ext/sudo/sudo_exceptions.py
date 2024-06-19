c152643e34b2215b629251a7a449301851a00a8c
Ersatz <cm12mc@gmail.com>
2024-06-19 04:05:07 -0700

    Ported the sudo cog to slash commands

--- commanderbot/ext/sudo/sudo_exceptions.py
+++ commanderbot/ext/sudo/sudo_exceptions.py
@@ -1,4 +1,4 @@
-from typing import Any, Optional
+from typing import Any
 
 from discord import Object
 from discord.ext.commands import Cog
@@ -13,36 +13,36 @@ class SudoException(ResponsiveException):
 class GlobalSyncError(SudoException):
     def __init__(self, reason: str):
         super().__init__(
-            f"**Unable to sync app commands globally**\n> Reason: `{reason}`"
+            f"😵 **Unable to sync app commands globally**\n> Reason: `{reason}`"
         )
 
 
 class GuildSyncError(SudoException):
     def __init__(self, guild: Object, reason: str):
         super().__init__(
-            f"**Unable to sync app commands to this guild**\n"
+            "😵 **Unable to sync app commands to this guild**\n"
             f"> Guild ID: `{guild.id}`\n"
             f"> Reason: `{reason}`"
         )
 
 
-class SyncUnknownGuild(SudoException):
-    def __init__(self, guild: Optional[Object]):
+class GuildIDNotFound(SudoException):
+    def __init__(self):
         super().__init__(
-            f"**Unknown guild**\n> Guild ID: `{guild.id if guild else '❓'}`"
+            "😬 A guild ID was not provided or a guild ID could not be found in the current context"
         )
 
 
 class UnknownCog(SudoException):
     def __init__(self, cog: str):
-        super().__init__(f"Unable to find a loaded cog with the name `{cog}`")
+        super().__init__(f"😔 Unable to find a loaded cog with the name `{cog}`")
 
 
 class CogHasNoStore(SudoException):
     def __init__(self, cog: Cog):
-        super().__init__(f"The cog `{cog.qualified_name}` does not use a store")
+        super().__init__(f"😬 The cog `{cog.qualified_name}` does not use a store")
 
 
 class UnsupportedStoreExport(SudoException):
     def __init__(self, store: Any):
-        super().__init__(f"Unsupported store export: `{type(store)}`")
+        super().__init__(f"😬 Unsupported store export: `{type(store)}`")
