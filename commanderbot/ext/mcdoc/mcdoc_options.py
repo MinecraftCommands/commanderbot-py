8bf339ec71e9fe83c62ae32889854226798f98a3
Misode <misoloo64@gmail.com>
2025-02-18 22:03:48 +0100

    Address review comments

--- commanderbot/ext/mcdoc/mcdoc_options.py
+++ commanderbot/ext/mcdoc/mcdoc_options.py
@@ -8,7 +8,7 @@ class McdocOptions(FromDataMixin):
     symbols_url: str
     manifest_url: str
     emoji_prefix: str
-    icon_url: Optional[str]
+    icon_url: Optional[str] = None
 
     @classmethod
     def try_from_data(cls, data: Any) -> Optional[Self]:
@@ -17,5 +17,5 @@ class McdocOptions(FromDataMixin):
                 symbols_url=data["symbols_url"],
                 manifest_url=data["manifest_url"],
                 emoji_prefix=data["emoji_prefix"],
-                icon_url=data.get("icon_url", None),
+                icon_url=data.get("icon_url"),
             )
