083ac330a55c9804d2ea766ca0dcb6026a09b05a
Ersatz <cm12mc@gmail.com>
2026-06-30 01:08:03 -0700

    Made custom Pydantic exceptions inherit from `ValueError`

--- commanderbot/lib/exceptions.py
+++ commanderbot/lib/exceptions.py
@@ -2,7 +2,6 @@ from typing import Any, Optional
 
 from discord import AllowedMentions, Interaction
 from discord.ext.commands import Context
-from pydantic_core import PydanticCustomError
 
 __all__ = (
     "MalformedData",
@@ -14,35 +13,28 @@ __all__ = (
 
 class MalformedData(Exception):
     def __init__(self, cls: type, data: Any):
-        super().__init__(f"Cannot create {cls.__name__} from {type(data).__name__}")
+        super().__init__(f"Cannot create '{cls.__name__}' from '{type(data).__name__}'")
 
 
-class NoSuchFactory(PydanticCustomError):
+class NoSuchFactory(ValueError):
     """
     Raised by a Pydantic validator if a factory function wasn't found.
     """
 
     def __init__(self, cls: type, factory_name: str):
         super().__init__(
-            "no_such_factory",
-            "'{class_name}' doesn't have a factory function called '{factory_name}'",
-            {"class_name": cls.__name__, "factory_name": factory_name},
+            f"'{cls.__name__}' doesn't have a factory function called '{factory_name}'",
         )
 
 
-class InstantiationError(PydanticCustomError):
+class InstantiationError(ValueError):
     """
     Raised by a Pydantic validator if there was an error while instantiating a class.
     """
 
     def __init__(self, cls: type, exception: Exception):
         super().__init__(
-            "instantiation_error",
-            "An error occurred while instantiating a '{class_name}': {message}",
-            {
-                "class_name": cls.__name__,
-                "message": str(exception),
-            },
+            f"An error occurred while instantiating a '{cls.__name__}': {str(exception)}",
         )
 
 
