bc43c5eedc7efb0d0637adeb458476bd3a8e5010
Ersatz <cm12mc@gmail.com>
2026-07-02 01:51:30 -0700

    Events no longer contain metadata

--- commanderbot/ext/automod/event/event_base.py
+++ commanderbot/ext/automod/event/event_base.py
@@ -1,8 +1,7 @@
 from abc import ABC
-from collections import defaultdict
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from logging import Logger
-from typing import Any, Optional
+from typing import Optional
 
 from discord import CategoryChannel, Member, Message, Reaction, Thread, User
 from discord.ext.commands import Bot
@@ -23,10 +22,6 @@ class AutomodEvent(ABC):
     state: AutomodGuildStateRef
     bot: Bot
     log: Logger
-
-    _metadata: defaultdict[str, dict] = field(
-        init=False, default_factory=lambda: defaultdict(dict)
-    )
 
     @property
     def channel(self) -> Optional[GuildChannel | Thread]:
@@ -74,11 +69,3 @@ class AutomodEvent(ABC):
     def user(self) -> Optional[User]:
         """Return the user-in-question, if any."""
         return None
-
-    def set_metadata(self, namespace: str, name: str, value: Any):
-        """Attach metadata to the event."""
-        self._metadata[namespace][name] = value
-
-    def remove_metadata(self, namespace: str, name: str):
-        """Remove metadata from the event."""
-        del self._metadata[namespace][name]
