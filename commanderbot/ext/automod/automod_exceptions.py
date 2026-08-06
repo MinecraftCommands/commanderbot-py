0abec6902ed94f01f494ffb03eb4d5cbaae93e76
Ersatz <cm12mc@gmail.com>
2026-08-06 01:31:04 -0700

    Update automod options and exceptions

---
+++ commanderbot/ext/automod/automod_exceptions.py
@@ -0,0 +1,83 @@
+from commanderbot.lib.exceptions import ResponsiveException
+
+
+class AutomodException(ResponsiveException):
+    pass
+
+
+class AutomodRuleAlreadyExists(AutomodException):
+    def __init__(self, name: str):
+        self.name: str = name
+        super().__init__(f"🤷 Automod rule `{self.name}` already exists")
+
+
+class AutomodRuleDoesNotExist(AutomodException):
+    def __init__(self, name: str):
+        self.name: str = name
+        super().__init__(f"😬 Automod rule `{self.name}` does not exist")
+
+
+class CannotModifyAutomodRuleName(AutomodException):
+    def __init__(self):
+        super().__init__(
+            "😬 Automod rule names can't be changed after the rule was created"
+        )
+
+
+class AutomodRuleAlreadyEnabled(AutomodException):
+    def __init__(self, name: str):
+        self.name: str = name
+        super().__init__(f"😬 Automod rule `{name}` is already enabled")
+
+
+class AutomodRuleAlreadyDisabled(AutomodException):
+    def __init__(self, name: str):
+        self.name: str = name
+        super().__init__(f"😬 Automod rule `{name}` is already disabled")
+
+
+class AutomodRuleMetadataAlreadyExists(AutomodException):
+    def __init__(self, name: str):
+        self.name: str = name
+        super().__init__(
+            f"😬 Automod rule metadata `{self.name}` already exists (This should never happen!!!)"
+        )
+
+
+class AutomodRuleMetadataDoesNotExist(AutomodException):
+    def __init__(self, name: str):
+        self.name: str = name
+        super().__init__(
+            f"🤷 Automod rule metadata `{self.name}` does not exist (This should never happen!!!)"
+        )
+
+
+class AutomodBucketAlreadyExists(AutomodException):
+    def __init__(self, name: str):
+        self.name: str = name
+        super().__init__(f"🤷 Automod bucket `{self.name}` already exists")
+
+
+class AutomodBucketDoesNotExist(AutomodException):
+    def __init__(self, name: str):
+        self.name: str = name
+        super().__init__(f"😬 Automod bucket `{self.name}` does not exist")
+
+
+class AutomodBucketAlreadyEnabled(AutomodException):
+    def __init__(self, name: str):
+        self.name: str = name
+        super().__init__(f"😬 Automod bucket `{name}` is already enabled")
+
+
+class AutomodBucketAlreadyDisabled(AutomodException):
+    def __init__(self, name: str):
+        self.name: str = name
+        super().__init__(f"😬 Automod bucket `{name}` is already disabled")
+
+
+class OCRNotSupported(AutomodException):
+    def __init__(self):
+        super().__init__(
+            "Automod does not support OCR. The feature is either not installed or misconfigured."
+        )
