f645322d17a48f1aa21bc19e78a3cb3687bff8b8
Ersatz <cm12mc@gmail.com>
2026-08-21 00:52:49 -0700

    Improved the code that submits tasks to the global process pool

--- commanderbot/ext/automod/conditions/image_attachments_contain.py
+++ commanderbot/ext/automod/conditions/image_attachments_contain.py
@@ -1,9 +1,11 @@
 import asyncio
 import unicodedata
 from itertools import chain
-from typing import Any, Literal, Optional, cast, override
+from logging import Logger
+from typing import Any, ClassVar, Literal, Optional, cast, override
 
-from pydantic import Field, PositiveInt
+import pebble
+from pydantic import Field, PositiveFloat, PositiveInt
 
 from commanderbot.core.utils import is_commander_bot
 from commanderbot.ext.automod.automod_context import AutomodContext
@@ -22,6 +24,8 @@ __all__ = ("ImageAttachmentsContain",)
 
 if SUPPORTS_OCR:
     from commanderbot.ext.automod.conditions.ocr_utils.pipeline import get_text
+
+MAX_OCR_WORKERS: int = 4
 
 
 class ImageAttachmentsContain(AutomodCondition):
@@ -65,8 +69,17 @@ class ImageAttachmentsContain(AutomodCondition):
     normalization_form: UnicodeNormalizationForms = Field(default="NFKD")
     """If enabled, the type of normalization to apply. Defaults to NFKD."""
 
-    timeout: PositiveInt = 10
+    timeout: PositiveFloat = 10
     """The max amount of time in seconds to wait for OCR results."""
+
+    _semaphore: ClassVar[Optional[asyncio.Semaphore]] = None
+    """Used to limit how many OCR workers get sumbitted to the pool."""
+
+    @classmethod
+    def get_semaphore(cls) -> asyncio.Semaphore:
+        if cls._semaphore is None:
+            cls._semaphore = asyncio.Semaphore(MAX_OCR_WORKERS)
+        return cls._semaphore
 
     @override
     def model_post_init(self, context: Any):
@@ -91,6 +104,40 @@ class ImageAttachmentsContain(AutomodCondition):
 
         return False
 
+    async def _ocr_worker(
+        self,
+        pool: pebble.ProcessPool,
+        log: Logger,
+        attachment_id: AttachmentID,
+        data: bytes,
+        lang: str,
+    ) -> Optional[tuple[str, AttachmentID]]:
+        future: Optional[pebble.ProcessFuture] = None
+        try:
+            async with self.get_semaphore():
+                future = pool.schedule(
+                    get_text, args=[data, lang, attachment_id], timeout=self.timeout
+                )
+                log.debug(
+                    f"OCR worker for attachment '{attachment_id}' was submitted to the pool"
+                )
+                result = await asyncio.wrap_future(future)
+                log.debug(f"OCR worker for attachment '{attachment_id}' is done")
+                return result
+        except TimeoutError:
+            log.warning(f"OCR worker for attachment '{attachment_id}' timed out")
+            return
+        except asyncio.CancelledError:
+            if future and not future.done():
+                future.cancel()
+            log.debug(f"OCR worker for attachment '{attachment_id}' was cancelled")
+            raise
+        except Exception:
+            log.exception(
+                f"OCR worker for attachment '{attachment_id}' ran into an error"
+            )
+            return
+
     async def _ocr(self, context: AutomodContext) -> Optional[AttachmentID]:
         # We need image attachments in context
         image_attachments = await context.fetch_attachments_with_type(*IMAGE_MIME_TYPES)
@@ -103,28 +150,32 @@ class ImageAttachmentsContain(AutomodCondition):
         # Submit OCR tasks to the process pool
         assert is_commander_bot(context.bot)
         pool = context.bot.pool
-        loop = asyncio.get_running_loop()
+        log = context.log
         tasks = [
-            loop.run_in_executor(pool, get_text, data, lang, attachment.id)
-            for (attachment, data, _) in image_attachments
+            asyncio.create_task(self._ocr_worker(pool, log, attachment.id, data, lang))
+            for attachment, data, _ in image_attachments
         ]
 
         # Check OCR results as they come in
         # This may raise a `TimeoutError`, but it *should* be caught in the guild state
         try:
-            async for completed_task in asyncio.as_completed(
-                tasks, timeout=self.timeout
-            ):
-                # Check if the text passes the condition and return the attachment
-                if result := completed_task.result():
-                    assert isinstance(result, tuple)
-                    text, attachment_id = result
-                    if self._check_ocr_result(text):
-                        return attachment_id
+            async for completed_task in asyncio.as_completed(tasks):
+                try:
+                    # Check if the text passes the condition and return the attachment ID
+                    if task_result := await completed_task:
+                        text, attachment_id = task_result
+                        if self._check_ocr_result(text):
+                            return attachment_id
+                except Exception:
+                    continue
         finally:
-            for task in tasks:
-                if not task.done():
-                    task.cancel()
+            # Cancel any remaining tasks
+            for t in tasks:
+                if not t.done():
+                    t.cancel()
+
+            # Wait for those tasks to be cleaned up
+            await asyncio.gather(*tasks, return_exceptions=True)
 
     @override
     async def check(self, context: AutomodContext) -> bool:
